0e5edf1d0792ab6a8cf76005c55245ec5c8bb930
Max Losch <M.M.Losch@uva.nl>
2018-06-13 16:28:24 +0200

    Update dataset links

--- scripts/download_pretrained_models.sh
+++ scripts/download_pretrained_models.sh
@@ -2,9 +2,9 @@
 
 MODELDST="data/models"
 mkdir -p "$MODELDST"
-wget -cO "$MODELDST/models.tar.bz2" "https://s3.eu-central-1.amazonaws.com/multitaskcnns/models.tar.bz2"
+wget -cO "$MODELDST/models.tar.bz2" "https://datasets.d2.mpi-inf.mpg.de/multitaskcnns/models.tar.bz2"
 tar -xvjf "$MODELDST/models.tar.bz2" -C "$MODELDST/" && rm "$MODELDST/models.tar.bz2"
 
 DATADST="data"
-wget -cO "$DATADST/mtldataset_test.tar.gz" "https://s3.eu-central-1.amazonaws.com/multitaskcnns/mtldataset_test.tar.gz"
+wget -cO "$DATADST/mtldataset_test.tar.gz" "https://datasets.d2.mpi-inf.mpg.de/multitaskcnns/mtldataset_test.tar.gz"
 tar -xvzf "$DATADST/mtldataset_test.tar.gz" -C "$DATADST/" && rm "$DATADST/mtldataset_test.tar.gz"
